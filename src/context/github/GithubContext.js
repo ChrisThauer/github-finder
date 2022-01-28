a5c4409cc2eee753379b7dfd1d89da79cc877a54
ChrisThauer <cthauerdev@gmail.com>
2022-01-28 10:59:03 -0800

    Added GetUser to Github Context and Reducer

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,7 +9,8 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
-    loading: false
+    loading: false,
+    user: {},
   }
 
   const [state, dispatch] = useReducer(githubReducer, initialState)
@@ -36,6 +37,28 @@ export const GithubProvider = ({ children }) => {
     })
   }
 
+  // Get single user
+  const getUser = async (login) => {
+    setLoading()
+
+    const res = await fetch(`${GITHUB_URL}/users/${login}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`
+      }
+    })
+
+    if (res.status === 404) {
+      window.location = '/notfound'
+    } else {
+      const data = await res.json()
+
+      dispatch({
+        type: 'GET_USER',
+        payload: data,
+      })
+    }
+  }
+
   const clearUsers = () => dispatch({
     type: 'CLEAR_USERS'
   })
@@ -47,8 +70,10 @@ export const GithubProvider = ({ children }) => {
 
   return <GithubContext.Provider value={{
     users: state.users,
+    user: state.user,
     loading: state.loading,
     searchUsers,
+    getUser,
     clearUsers
   }}>
     {children}
